b03078fe864c286e3cbf7244497ddae2319b1fee
Rithik Raja S <rithikraja28.rr@gmail.com>
2024-06-07 12:48:00 +0530

    failure

--- android/app/src/main/kotlin/com/rithik/candy_ai/candy_ai/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.rithik.candy_ai.candy_ai
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/secret.dart
+++
@@ -1,1 +0,0 @@
-const openAIAPIKey = 'sk-proj-Qf0dW8giSyyTMWK6aIp0T3BlbkFJMSuYk0j1EGhHE4J7bFll';
